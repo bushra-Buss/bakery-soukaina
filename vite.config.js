353d027134468249d089e6d85eba2c9bc3f8bbb8
Ahmad Kadri <ahmadkadri.87@hotmail.com>
2025-12-09 19:46:59 +0100

    add ignore

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/soukaina-bakery/',
+  base: '/bakery-soukaina/',
   plugins: [react()],
 })
